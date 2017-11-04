81a925f549cd263acff323af4e3280066556bbc1
Greg Look <greg@greg-look.net>
2017-11-04 13:36:42 -0700

    Bump version, uprgade puget.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject lein-cprint "1.2.0"
+(defproject lein-cprint "1.2.1-SNAPSHOT"
   :description "Like lein-pprint, but colorized!"
   :url "https://github.com/greglook/lein-cprint"
   :license {:name "Public Domain"
             :url "http://unlicense.org/"}
 
+  :deploy-branches ["master"]
   :eval-in-leiningen true
 
-  :dependencies [[mvxcvi/puget "1.0.0"]])
+  :dependencies
+  [[mvxcvi/puget "1.0.2"]])
